77dd3ecbd4aec491cd143dc7024bb892cd0e916a
FernLikesSushi <fernlikessushi@noreply.codeberg.org>
2026-08-19 20:37:20 -0400

    Switch to module css

--- src/components/Toggle.tsx
+++ src/components/Toggle.tsx
@@ -1,8 +1,8 @@
-import "./Toggle.css";
+import styles from "./Toggle.module.css";
 
 export function Toggle(props: { label: string; checked: boolean; onChange: (checked: boolean) => void }) {
   return (
-    <label class="toggle">
+    <label class={styles.toggle}>
       <input type="checkbox" checked={props.checked} onChange={(e) => props.onChange(e.currentTarget.checked)} />
       {props.label}
     </label>
