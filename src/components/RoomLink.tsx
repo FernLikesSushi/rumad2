0185dea0a6429c43d5516ddab51cfa19936375a4
FernLikesSushi <fernlikessushi@noreply.codeberg.org>
2026-08-31 13:56:56 -0400

    Sanitize room link to avoid weird encoding

--- src/components/RoomLink.tsx
+++ src/components/RoomLink.tsx
@@ -3,7 +3,12 @@ import { createMemo } from "solid-js";
 export function RoomLink(props: { room: string; text?: string; class?: string }) {
     // TODO: Handle roomless courses (e.g. online courses)
 
-    const link = createMemo(() => `/map/${props.room}`);
+    function sanitizeRoomCode(room: string) {
+        // Remove any whitespace and convert to uppercase
+        return room.replace(/\s+/g, "").toUpperCase();
+    }
+
+    const link = createMemo(() => `/map/${sanitizeRoomCode(props.room)}`);
     const classes = createMemo(() => props.class ?? "link link-secondary");
 
     return (
